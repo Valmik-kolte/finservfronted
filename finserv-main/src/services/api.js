7d1dd30c8b495dae0fbb1ed8e4609dbedce0d24b
caryanamPratikp <pratikpatilcaryanamindia.in>
2026-06-09 10:27:58 +0530

    api.js updated

--- finserv-main/src/services/api.js
+++ finserv-main/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api",
+  baseURL: "https://v1.vahanfinserv.com/api",
 });
 
 // Attach JWT token automatically
